2f042ded4929fce25128397b021b78368490cc7e
YCHULHEE <ych9411@naver.com>
2021-01-03 21:57:08 +0900

    HTTP Method Update.

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,35 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") //
+    public String getRequest(){
+
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") // localhost:8080/api/getParameter?id1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam(name = "passsword") String pwd) {
+        System.out.println(("id : " + id));
+        System.out.println(("pwd : " + pwd));
+
+        return id+pwd;
+    }
+
+    // localhost:8080/api/multiParameter?account=abcd&email=study@gmail.com&page=10
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam) {
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+
+        // { "account" : "", "email" : "", "page" : 0}
+        return searchParam;
+    }
+}
