87338d3616fec89dc173b1b2c355d125f011e01e
YCHULHEE <ych9411@naver.com>
2021-01-03 22:24:51 +0900

    POST Method Update.

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+
+    // HTML <Form>
+    // ajax 검색
+    // http post body -> data
+    // json, xml, multipart.form / text-plain
+
+    @PostMapping(value = "/postMethod")
+    public String postMethod(@RequestBody SearchParam searchParam) {
+        return "OK";
+    }
+
+    @PutMapping
+    public void put() {
+
+    }
+
+    @PatchMapping
+    public void patch() {
+        
+    }
+}
